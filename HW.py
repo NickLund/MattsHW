0b78041038b686d39f1fc50df3a71111050388d6
NickLund <trojanscout@gmail.com>
2020-01-28 09:57:40 -0700

    Update HW.py
    Ok basically almost fully automated, update the list to include number of problems
    Create function to print homework assignment number
    Function that calls 2 functions to print question number and then run a function for that question.
    Moved the actual question into a function

--- HW.py
+++ HW.py
@@ -6,6 +6,7 @@ Created on Mon Jan 27 20:23:58 2020
 @author: Matthew
 """
 
+# While this is a cleaner version of HW 6, I am over making this, so that we can use it as a template for future labs/HW
 #!/usr/bin/env python
 #
 # Name: Mathew Bechtel
@@ -15,21 +16,42 @@ Created on Mon Jan 27 20:23:58 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-print('============')
-print(' Homework 6')
-print('============'
+# List object containing the function names of all HW problems
+problemFunctions = [prob1, prob2, prob3, prob4]
 
-# -----------
-# Problem 4
-# -----------
-
-print('')
-print('---------')
-print('Problem 4')
-print('---------')
-
-for z in range(0, 30001, 1000):
-  def P(z):
+# Problem num 4
+def prob4():
+  for z in range(0, 30001, 1000):
     P = 1 * np.exp(((-29 * 9.81 * z)/(8.3145 * 300))/1000)
-    return P
-  print(z, P(z))
+    print(z, P)
+
+# Prints the problem number (passed as int)
+def problemPrint(prob):
+  print('')
+  print("---------")
+  print("Problem %d" % (prob))
+  print("---------')
+
+# Function first runs funtion to print problem number, then grabs correct function from list to run
+def questionNum(numb):
+  problemPrint(numb)
+  problemFunctions[numb]()
+  
+# Prints the homework version, modify by changing the int in the main function
+def homeworkNumPrint(hw):
+  print("============")
+  print(" Homework %d" % (hw))
+  print("============")
+        
+def main():
+  #Change the number for the assignment number
+  homeworkNumPrint(6)
+  numberProbs = problemFunctions.size()
+# Uncomment one of the two, 1st for testing/running specific problem(s), 2nd for running all problems
+# for i in range(1, numberProbs):
+  for i in range(4, 4):
+    questionNum(i)
+    
+      
+if __name__ == "__main__":
+      main()
